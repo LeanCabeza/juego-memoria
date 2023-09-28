51f53b83df3d6c7027e62a5e8eaedd51f4b5c0b5
Leandro Cabeza <cabezaleandro@gmail.com>
2023-09-28 17:59:45 -0300

    Added splash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    loadChildren: () => import('./paginas/splash/splash.module').then( m => m.SplashPageModule)
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'login',
+    loadChildren: () => import('./paginas/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./paginas/home/home.module').then( m => m.HomePageModule)
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrts9hTdo-aQEfbKdvqme_fhjM4PfhKFU",
+  authDomain: "pps-2023-3661d.firebaseapp.com",
+  projectId: "pps-2023-3661d",
+  storageBucket: "pps-2023-3661d.appspot.com",
+  messagingSenderId: "759605409724",
+  appId: "1:759605409724:web:ad60abe94eff4a26585db9"
+};
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(firebaseConfig),AngularFireAuthModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
